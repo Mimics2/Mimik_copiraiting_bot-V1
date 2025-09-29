02ebd25965417d652786c32c3444853a5de2c31d
Mimics2 <kolesnicenkauoevsej@gmail.com>
2025-09-29 17:18:12 +0500

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-
+# config (1) (1).py
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8331986255:AAH6Y0ELNanUc0Ae7gD0qLh3A-tf-cH5V4E')
+# Если на Railway переменная BOT_TOKEN не установлена, будет использован ваш новый токен.
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8331986255:AAH6Y0ELNanUc0Ae7gD0qLh3A-tf-cH5V4E') 
 ADMIN_IDS = [int(x) for x in os.getenv('ADMIN_IDS', '6646433980').split(',') if x]
